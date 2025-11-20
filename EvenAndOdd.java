af46359ff81bb8e635a3289d736cc730aa5a1c73
Farooque <farooqueshaikh7499@gmail.com>
2025-11-20 22:28:49 +0530

    Even & Odd No

---
+++ EvenAndOdd.java
@@ -0,0 +1,20 @@
+package com.ferry.codingpractice.basic;
+
+public class EvenAndOdd {
+
+	public static void main(String[] args) {
+
+		int num = 102;
+		
+		if(num%2 == 0)
+		{
+			System.out.println("Number is Even: " + num);
+		}
+		
+		else
+		{
+			System.out.println("Number is Odd: " + num);
+		}
+	}
+
+}
